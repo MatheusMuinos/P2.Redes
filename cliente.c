da54580fbe5aeb366c819c4dfb9c382d7910201d
WailingSpud <sebastian.montoto@rai.usc.es>
2025-10-01 09:46:36 +0200

    Borrado

--- cliente.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <inttypes.h>
-#include <netinet/in.h>
-#include <netdb.h>
-#include <arpa/inet.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include "funciones.c"
